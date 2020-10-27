4aadeec837c6fbea2d49c887d4d9133d8b1a4c68
William Purnell <william.purnell@students.plymouth.ac.uk>
2020-10-27 16:07:47 +0000

    Add initial empty tests

---
+++ OnlineChatSystem_Test.js
@@ -0,0 +1,22 @@
+suite("Basic Chat Functionality", function(){
+    setup(function(){
+        this.newMessage = "message";
+    });
+
+    test("SendMessage", function(){
+
+    });
+
+    test("RecieveMessage", function(){
+
+    });
+
+    test("GetDraftMessage", function(){
+
+    });
+
+    test("SetDraftMessage", function(){
+
+    });
+
+})
